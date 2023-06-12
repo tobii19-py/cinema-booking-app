ba0f8a43ace991c56c271bd348d2fa8d1b980d22
tobi <tobibags19@gmail.com>
2023-06-12 06:27:44 -0500

    Implement pdf ticket method

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import fpdf
+import string
+import random
+from fpdf import FPDF
 import sqlite3
 
 
 class User:
+    """Represents a user who is able to purchase a movie showing"""
 
     def __init__(self, name):
         self.name = name
@@ -86,20 +89,46 @@ class Card:
 
 
 class Ticket:
-    def id(self):
-        pass
-
-    def user(self):
-        pass
-
-    def price(self):
-        pass
-
-    def seat(self):
-        pass
+    """Represents a cinema ticket purchased by a User"""
+    def __init__(self, user, price, seat_number):
+        self.user = user
+        self.price = price
+        self.seat_number = seat_number
+        self.id = "".join([random.choice(string.ascii_letters) for i in range(8)])
 
     def to_pdf(self, path):
-        pass
+        """Creates a PDF for movie ticket"""
+        pdf = FPDF(orientation="P", unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.cell(w=0, h=80, txt="Your Digital Ticket", border=1, ln=1, align="C")
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Name: ", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=self.user.name, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Ticket ID", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=self.id, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Price", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=str(self.price), border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Seat Number", border=1)
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.cell(w=0, h=25, txt=str(self.seat_number), border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.output("sample.pdf", 'F')
 
 
 name = input("Enter your full name: ")
